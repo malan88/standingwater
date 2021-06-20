35b148cd88414edb9da8fd0af1684ba2efce3932
malan88 <m@stdwtr.io>
2021-06-19 20:28:15 -0400

    I think projects is sufficient

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,7 +4,7 @@ import { graphql } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import styled from 'styled-components';
 import order from '../assets/projects/sort';
-import { faMicrochip, faRobot, faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { faUndo, faMicrochip, faRobot, faGlobe } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const categories = {
@@ -12,14 +12,33 @@ const categories = {
   web: faGlobe,
   hardware: faMicrochip
 }
+const IconFrame = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: fit-content;
+  flex-direction: column;
+  align-items: center;
+  margin: 2rem;
+  color: ${(props) => props.theme.colors.Blue};
+  cursor: pointer;
+  &:hover {
+    color: ${(props) => props.theme.colors.Sienna};
+    transition: color 0.35s ease-out;
+  }
+  &.active {
+    color: ${(props) => props.theme.colors.Green};
+  }
+`;
+const Icon = styled(FontAwesomeIcon)`
+`;
 
 const Image = styled(GatsbyImage)`
   margin: 0 auto;
-`
+`;
 const Text = styled.div`
   width: 85%;
   margin: 2rem auto;
-`
+`;
 const Box = styled.div`
   font-family: ${(props) => props.theme.fonts.Text};
   border-color: ${(props) => props.theme.colors.Charcoal};
@@ -33,7 +52,7 @@ const Box = styled.div`
   h1 {
     margin: 1rem 0;
   }
-`
+`;
 const Pill = styled.button`
   border-radius: 50px;
   background-color: ${(props) => props.theme.colors.Amber};
@@ -48,13 +67,13 @@ const Pill = styled.button`
   &.active {
     background-color: ${(props) => props.theme.colors.Green};
   }
-`
+`;
 const Tags = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   font-family: ${(props) => props.theme.fonts.Text};
-`
+`;
 
 const Project = (props) => {
   return (
@@ -71,8 +90,14 @@ const Project = (props) => {
             </a>
         }
         <Text>
-          {props.tags.map((item, i) => (
-            <Pill key={item + props.title}>{item}</Pill>
+          {props.tags.map((tag) => (
+            <Pill
+              key={tag + props.title}
+              className={props.selectedTags.includes(tag) ? "active" : ""}
+              onClick={() => props.clickTagHandler(tag)}
+            >
+              {tag}
+            </Pill>
           ))}
           <h1><a href={props.url}><FontAwesomeIcon icon={categories[props.category]}/> {props.title}</a></h1>
           <div dangerouslySetInnerHTML={{__html: props.body}} />
@@ -91,48 +116,97 @@ class ProjectPage extends React.Component {
     nodes.sort((a,b) => { return order.indexOf(a.node.frontmatter.short) - order.indexOf(b.node.frontmatter.short); });
 
     // remove nodes not in order and add all tags
-    const tags = new Set();
-    for (let i = 0; i < nodes.length; i++) {
+    for (let i = 0; i < nodes.length; i++)
       if (!order.includes(nodes[i].node.frontmatter.short)) nodes.splice(i,1);
+
+    // create tag list and category tree map
+    const tags = new Set();
+    const categoryTagMap = {}
+    Object.keys(categories).forEach((cat) => categoryTagMap[cat] = []); // initialize map
+    for (let i = 0; i < nodes.length; i++) {
+      const node = nodes[i].node;
+      node.frontmatter.tags.forEach((tag) => {
+        tags.add(tag);
+        if (!categoryTagMap[node.frontmatter.category].includes(tag)) categoryTagMap[node.frontmatter.category].push(tag);
+      })
     }
+    this.categoryTagMap = categoryTagMap;
     nodes.forEach(({node}) => node.frontmatter.tags.forEach((tag) => tags.add(tag)));
+    this.tags = [...tags];
+    this.tags.sort();
     this.nodes = nodes;
     this.state = {
       visible: nodes,
       selectedTags: [],
-      selectedCategories: [],
+      selectedCategory: '',
+      visibleTags: this.tags,
+      visiblePool: nodes,
     };
-    this.tags = [...tags];
-    this.tags.sort();
   }
 
   clickTagHandler = (tag) => {
     const sel = this.state.selectedTags.slice();
-    sel.push(tag);
+    if (sel.includes(tag)) {
+      const index = sel.indexOf(tag);
+      if (index > -1) sel.splice(index, 1);
+    } else sel.push(tag);
     this.setState({selectedTags: sel}, () => this.filterTags());
   }
 
   filterTags = () => {
-    const visible = this.nodes.slice();
+    console.log('filterTags');
+    const visible = this.state.visiblePool.slice();
     const sel = this.state.selectedTags.slice();
-    const newvisible = [];
-    visible.forEach((node) => {
-      for (let tag of node.node.frontmatter.tags) {
-        if (sel.includes(tag)) {
-          newvisible.push(node);
-          break;
+    let newvisible = [];
+
+    if (sel.length <= 0) newvisible = visible;
+    else
+      visible.forEach((node) => {
+        for (let tag of node.node.frontmatter.tags) {
+          if (sel.includes(tag)) {
+            newvisible.push(node);
+            console.log(node.node.frontmatter.title)
+            break;
+          }
         }
-      }
-    });
+      });
     this.setState({visible: newvisible})
   }
+
+  clickCategoryHandler = (category) => {
+    if (this.state.selectedCategory===category) this.setState({selectedCategory: "", visibleTags: this.tags}, () => this.filterCategory());
+    else this.setState({selectedCategory: category, visibleTags: this.categoryTagMap[category]}, () => this.filterCategory());
+  }
+  filterCategory = () => {
+    const nodes = this.nodes.slice();
+    let newvisible = [];
+    if (this.state.selectedCategory !== "")
+      nodes.forEach((node) => {
+        if (node.node.frontmatter.category === this.state.selectedCategory) newvisible.push(node);
+      })
+    else newvisible = this.nodes;
+    this.setState({visible: newvisible, visiblePool: newvisible});
+  }
+
 
   render() {
     return (
       <Layout>
         <main>
           <Tags>
-            {this.tags.map((tag) => (
+            {Object.keys(categories).map((category) => (
+              <IconFrame
+                className={this.state.selectedCategory === category ? "active" : ""}
+                onClick={() => this.clickCategoryHandler(category)}
+                key={category + "root"}
+              >
+                <Icon size="3x"icon={categories[category]} />
+                {category}
+              </IconFrame>
+            ))}
+          </Tags>
+          <Tags>
+            {this.state.visibleTags.map((tag) => (
               <Pill
                 className={this.state.selectedTags.includes(tag) ? "active" : ""}
                 onClick={() => this.clickTagHandler(tag)}
@@ -151,7 +225,8 @@ class ProjectPage extends React.Component {
                 url={node.frontmatter.url}
                 category={node.frontmatter.category}
                 tags={node.frontmatter.tags}
-                activeTags={this.state.selectedTags}
+                selectedTags={this.state.selectedTags}
+                clickTagHandler={this.clickTagHandler}
               />
             )
           )}
