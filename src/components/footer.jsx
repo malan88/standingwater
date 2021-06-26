9d5a212a973b87d85f05f66fa7236a551914ef94
malan88 <m@stdwtr.io>
2021-06-26 14:55:33 -0400

    Add copyright

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,50 +1,65 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import {
   faGithub,
   faStackOverflow,
   faHackerrank,
-  faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+  faKeybase,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { breakpoints } from "../global/breakpoints";
 
 const links = {
-  "GitHub": [faGithub, "https://github.com/malan88"],
-  "StackOverflow": [faStackOverflow, "https://stackoverflow.com/story/malan88"],
-  "HackerRank": [faHackerrank, "https://www.hackerrank.com/mas88?hr_r=1"],
-  "LinkedIn": [faLinkedin, "https://www.linkedin.com/michael-sendker"],
-  "Résumé": [faFilePdf, "https://raw.githubusercontent.com/malan88/resume/master/main.pdf"],
-}
+  GitHub: [faGithub, "https://github.com/malan88"],
+  StackOverflow: [faStackOverflow, "https://stackoverflow.com/story/malan88"],
+  Keybase: [faKeybase, "https://keybase.io/m88"],
+  HackerRank: [faHackerrank, "https://www.hackerrank.com/mas88?hr_r=1"],
+  LinkedIn: [faLinkedin, "https://www.linkedin.com/michael-sendker"],
+  Résumé: [
+    faFilePdf,
+    "https://raw.githubusercontent.com/malan88/resume/master/main.pdf",
+  ],
+};
 
-const Bar = styled.div`
-  display: flex;
-  width: 30%;
+const Width = styled.div`
+  width: 40%;
   margin: 5rem auto;
-  justify-content: space-between;
   ${breakpoints.vp12} {
-    width: 60%;
+    width: 70%;
   }
   ${breakpoints.vp4} {
-    width: 80%;
+    width: 90%;
   }
   ${breakpoints.vp3} {
-    width: 95%;
+    width: 100%;
   }
-`;
-const Icon = styled(FontAwesomeIcon)`
 `
+
+const Bar = styled(Width)`
+  display: flex;
+  justify-content: space-between;
+`;
+const Icon = styled(FontAwesomeIcon)``;
 
 const Footer = () => {
   return (
-    <Bar>
-      {Object.keys(links).map((key) => (
-        <a key={key} href={links[key][1]}>
-          <Icon title={key} icon={links[key][0]} size="2x" />
-        </a>
-      ))}
-    </Bar>
-  )
-}
+    <footer>
+      <Bar>
+        {Object.keys(links).map((key) => (
+          <a key={key} href={links[key][1]}>
+            <Icon title={key} icon={links[key][0]} size="2x" />
+          </a>
+        ))}
+      </Bar>
+      <Width>
+        © {new Date().getFullYear()} Michael Sendker,{" "}
+        <a href="https://standingwater.io">standingwater.io</a>, built with{" "}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </Width>
+    </footer>
+  );
+};
 
 export default Footer;
